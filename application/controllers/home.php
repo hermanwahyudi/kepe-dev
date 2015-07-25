bafeabd94d0b7cbf22d80b4c057f2074db0facdc
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-07-25 15:49:36 +0700

    Update error db_debug handling on production

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -46,6 +46,7 @@ class Home extends CI_Controller {
 		
 		$data = array_merge($content, $data);
 		
+		$this->output->cache(15); // cache about 15 minutes
 		$this->parser->parse('/index', $data);
 	}
 	
